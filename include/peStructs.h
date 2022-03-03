9a7c098a3293bcf75476ec213a314c4b1594afaa
Rebraws <Rebraws@protonmail.com>
2022-03-03 12:31:34 -0300

    Disable padding for IMAGE_FILE_HEADER

--- include/peStructs.h
+++ include/peStructs.h
@@ -23,7 +23,7 @@ namespace pelf {
 using WORD = std::uint16_t;
 using DWORD = std::uint32_t;
 
-
+#pragma pack(push, 1)     // Disable padding 
 struct IMAGE_FILE_HEADER {
 	BOOST_HANA_DEFINE_STRUCT(IMAGE_FILE_HEADER,
   	(WORD,  Machine),
@@ -35,34 +35,35 @@ struct IMAGE_FILE_HEADER {
   	(WORD,  Characteristics)
 	);
 
-	[[nodiscard]] constexpr auto getMachine() -> WORD { 
+	[[nodiscard]] constexpr auto getMachine() const -> WORD { 
 		return hana::at_c<0>(hana::members(*this)); 
 	}
 
-	[[nodiscard]] constexpr auto getNumberOfSections() -> WORD {
+	[[nodiscard]] constexpr auto getNumberOfSections() const -> WORD {
 		return hana::at_c<1>(hana::members(*this)); 
 	}
 
-	[[nodiscard]] constexpr auto getTimeDateStamp() -> DWORD {
+	[[nodiscard]] constexpr auto getTimeDateStamp() const -> DWORD {
 		return hana::at_c<2>(hana::members(*this)); 
 	}
 
-	[[nodiscard]] constexpr auto getPointerToSymbolTable() -> DWORD {
+	[[nodiscard]] constexpr auto getPointerToSymbolTable() const -> DWORD {
 		return hana::at_c<3>(hana::members(*this));
 	}
 
-	[[nodiscard]] constexpr auto getNumberOfSymbols() -> DWORD {
+	[[nodiscard]] constexpr auto getNumberOfSymbols() const -> DWORD {
 		return hana::at_c<4>(hana::members(*this));
 	}
 
-	[[nodiscard]] constexpr auto getSizeOfOptionalHeader() -> WORD {
+	[[nodiscard]] constexpr auto getSizeOfOptionalHeader() const -> WORD {
 		return hana::at_c<5>(hana::members(*this));
 	}
 
-	[[nodiscard]] constexpr auto getCharacteristics() -> WORD {
+	[[nodiscard]] constexpr auto getCharacteristics() const -> WORD {
 		return hana::at_c<6>(hana::members(*this));
 	}
 };
+#pragma pack(pop)
 
 } // namespace end 
 
