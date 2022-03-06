a43d9f48291b0c06794264cb798ee6209f2bee12
Rebraws <Rebraws@protonmail.com>
2022-03-06 15:43:10 -0300

    Move readHeader() from Pe class to Pelf class, and replaced memcpy with std::copy()

--- include/Pe.h
+++ include/Pe.h
@@ -1,7 +1,7 @@
-/** @file Pe.h 
+/** @file Pe.h
  *  @brief Pe class declaration
  *
- *  This file contains the Pe class declaration 
+ *  This file contains the Pe class declaration
  *
  *
  *  @author Rebraws
@@ -21,20 +21,19 @@
 namespace pelf {
 
 
-
-/** @brief Pe class 
+/** @brief Pe class
  *
  *  @tparam Container The type of the container used to store the file content
- *  that needs to be parsed  
+ *  that needs to be parsed
  *
  *
  * */
 
-template <class Container>
-class Pe : public Pelf<Container, Pe> {
+template<class Container>
+class Pe : public Pelf<Container, Pe>
+{
 public:
-
-  /** @brief Pe constructor 
+  /** @brief Pe constructor
    *
    *  @param data data to be parsed
    * */
@@ -43,32 +42,30 @@ public:
 
   [[nodiscard]] constexpr auto getOptionalHeader() const -> OptionalHeader;
 
-  /** @brief Returns the Coff header 
+  /** @brief Returns the Coff header
    *
-   *  @return returns a `IMAGE_FILE_HEADER` struct which is a 
+   *  @return returns a `IMAGE_FILE_HEADER` struct which is a
    *  hana struct that represents the coff header of the PE file
    * */
   [[nodiscard]] constexpr auto getCoffHeader() const -> IMAGE_FILE_HEADER;
 
 
   [[nodiscard]] constexpr auto getStandardCoffFields() const -> StandardCoffFields;
-  
+
   [[nodiscard]] constexpr auto getWindowsSpecificFields() const -> WindowsSpecificFields;
-
-
 
 
 private:
   friend class Pelf<Container, Pe>;
 
-  static constexpr std::uint16_t  mMZDSignature{0x4d5a};    /**< MZ DOS Signature */
-  static constexpr std::uint32_t  mPeSignature{0x50450000};  /**< PE Signature 'PE\0\0' */
-  static constexpr std::uint8_t   mMinPeSize{97};           /**< Minimum possible PE file size */
-  
+  static constexpr std::uint16_t mMZDSignature{ 0x4d5a }; /**< MZ DOS Signature */
+  static constexpr std::uint32_t mPeSignature{ 0x50450000 }; /**< PE Signature 'PE\0\0' */
+  static constexpr std::uint8_t mMinPeSize{ 97 }; /**< Minimum possible PE file size */
+
   std::uint32_t mPeHeaderAddress{}; /**< 32 bit value that represents the start address of the coff header */
 
   IMAGE_FILE_HEADER mCoffHeader{}; /**< Struct that represents the coff header */
-  OptionalHeader    mOptionalHeader; /**< Struct that represents the optional header, */ 
+  OptionalHeader mOptionalHeader; /**< Struct that represents the optional header, */
 
 
   /** @brief  Checks if MZ DOS signature and PE signature are valid
@@ -78,23 +75,23 @@ private:
   [[nodiscard]] constexpr auto checkSignatures() const -> bool;
 
 
-  /** @brief Checks if the MZ DOS Signature (0x5a4d) at offset 0 is valid 
+  /** @brief Checks if the MZ DOS Signature (0x5a4d) at offset 0 is valid
    *
    *  @return Returns `true` if the signature is valid, `false` otherwise.
    * */
   [[nodiscard]] constexpr auto checkMZDSignature() const -> bool;
-  
+
   /** @brief Checks if the PE Signature `PE\0\0` is valid
    *
    *  @return Returns `true` if the signature is valid, `false` otherwise.
    * */
   [[nodiscard]] constexpr auto checkPESignature() const -> bool;
 
-    /** @brief Checks if the size is valid
+  /** @brief Checks if the size is valid
    *
    *  @return Returns `true` if the size is valid, and `false` otherwise
    * */
-  [[nodiscard]] constexpr auto checkFileSize() const -> bool;  
+  [[nodiscard]] constexpr auto checkFileSize() const -> bool;
 
 
   /** @brief Parses the Coff Header, and the optional coff header
@@ -104,134 +101,107 @@ private:
    * */
   constexpr auto parseHeaders() -> void;
 
-  template <class Header>
-  constexpr auto readHeader(Header& header,const std::size_t offset) -> void;
+};
 
-  
-
-};  
-
-template <class Container>
-constexpr Pe<Container>::Pe(Container data) :
-  Pelf<Container, Pe>(std::move(data)) {
+template<class Container>
+constexpr Pe<Container>::Pe(Container data) : Pelf<Container, Pe>(std::move(data))
+{
 
   if (!checkFileSize()) {
-    throw PelfException{"Invalid PE file size!"};
+    throw PelfException{ "Invalid PE file size!" };
   }
 
-  std::size_t offset{0x3c};
+  std::size_t offset{ 0x3c };
   for (std::size_t i{}; i < sizeof(mPeHeaderAddress); ++i) {
     mPeHeaderAddress <<= 8;
     mPeHeaderAddress |= static_cast<std::uint32_t>(
-        this->mData[sizeof(mPeHeaderAddress) - 1 + offset--]);  
+      this->mData[sizeof(mPeHeaderAddress) - 1 + offset--]);
   }
 
   this->parse();
-
 }
 
-template <class Container>
-constexpr auto Pe<Container>::checkMZDSignature() const -> bool {
+template<class Container>
+constexpr auto Pe<Container>::checkMZDSignature() const -> bool
+{
 
-  return (this->mData[0] == (mMZDSignature >> 8) &&
-         this->mData[1] == (mMZDSignature & 0xFF));
+  return (this->mData[0] == (mMZDSignature >> 8) && this->mData[1] == (mMZDSignature & 0xFF));
 }
 
-template <class Container>
-constexpr auto Pe<Container>::checkPESignature() const -> bool {
-  /* Read PE Signature from offset mPeHeaderAddress*/ 
+template<class Container>
+constexpr auto Pe<Container>::checkPESignature() const -> bool
+{
+  /* Read PE Signature from offset mPeHeaderAddress*/
   std::uint32_t pe_signature{};
   std::size_t offset = mPeHeaderAddress;
 
   /* mPeSignature is set as 0x5045000, so no need to read the value backwards */
   for (std::size_t i{}; i < sizeof(pe_signature); ++i) {
     pe_signature <<= 8;
-    pe_signature |= this->mData[offset++]; 
+    pe_signature |= this->mData[offset++];
   }
 
 
-  return pe_signature == mPeSignature; 
-    
+  return pe_signature == mPeSignature;
 }
 
 
-template <class Container>
-constexpr auto Pe<Container>::checkSignatures() const -> bool {
+template<class Container>
+constexpr auto Pe<Container>::checkSignatures() const -> bool
+{
   return checkMZDSignature() && checkPESignature();
 }
 
-template <class Container>
-constexpr auto Pe<Container>::checkFileSize() const -> bool {
+template<class Container>
+constexpr auto Pe<Container>::checkFileSize() const -> bool
+{
   return this->mData.size() >= mMinPeSize;
 }
 
 
-template <class Container>
-constexpr auto Pe<Container>::parseHeaders() -> void {
-  
-  std::size_t offset = mPeHeaderAddress + 4;
-  readHeader(mCoffHeader, offset);
+template<class Container>
+constexpr auto Pe<Container>::parseHeaders() -> void
+{
+
+  std::ptrdiff_t offset = mPeHeaderAddress + 4;
+  this->readHeader(mCoffHeader, offset);
 
   offset += sizeof(mCoffHeader);
-  readHeader(mOptionalHeader.mScf, offset);
+  this->readHeader(mOptionalHeader.mScf, offset);
 
   offset += sizeof(mOptionalHeader.mScf);
 
-  readHeader(mOptionalHeader.mWsf, offset);
+  this->readHeader(mOptionalHeader.mWsf, offset);
 
   /* Maybe a for loop for the IMAGE DATA DIRECTORY ARRAY */
-
-  
-
 }
 
 
-template <class Container>
-constexpr auto Pe<Container>::getCoffHeader() const -> IMAGE_FILE_HEADER {
+template<class Container>
+constexpr auto Pe<Container>::getCoffHeader() const -> IMAGE_FILE_HEADER
+{
   return mCoffHeader;
 }
 
-template <class Container>
-constexpr auto Pe<Container>::getStandardCoffFields() const -> StandardCoffFields {
+template<class Container>
+constexpr auto Pe<Container>::getStandardCoffFields() const -> StandardCoffFields
+{
   return mOptionalHeader.mScf;
 }
 
-template <class Container>
-constexpr auto Pe<Container>::getWindowsSpecificFields() const -> WindowsSpecificFields {
+template<class Container>
+constexpr auto Pe<Container>::getWindowsSpecificFields() const -> WindowsSpecificFields
+{
   return mOptionalHeader.mWsf;
 }
 
-template <class Container>
-constexpr auto Pe<Container>::getOptionalHeader() const -> OptionalHeader {
+template<class Container>
+constexpr auto Pe<Container>::getOptionalHeader() const -> OptionalHeader
+{
   return mOptionalHeader;
 }
 
 
-
-
-
-/* PROBABLY NEED TO MOVE THIS INTO PELF SINCE ELF CLASS IS GOING TO USE
- * THIS FUNCTION TOO */
-template <class Container>
-template<class Header>
-constexpr auto Pe<Container>::readHeader(Header& header,const std::size_t offset) -> void {
-
-  if (std::is_constant_evaluated()) {
-    header = this->template getStruct<Header>(offset);
-  } else {
-
-    assert(std::is_trivially_copyable<Header>::value);
-    
-    if (offset + sizeof(header) < this->mData.size()) {
-      std::memcpy(reinterpret_cast<char *>(&header), 
-          this->mData.data() + offset, sizeof(header));
-    }else {
-      throw PelfException{"Invalid Header"};
-    }
-
-  }
-}
-
-} // end of namespace
+}// namespace pelf
 
 #endif
--- include/Pelf.h
+++ include/Pelf.h
@@ -2,7 +2,7 @@
  *  @brief Pelf class declaration
  *
  *  This file contains the Pelf class declaration, which is the base class
- *  of the Pe and Elf classes  
+ *  of the Pe and Elf classes
  *
  *
  *  @author Rebraws
@@ -20,25 +20,18 @@
 #include <boost/hana.hpp>
 
 
-
 namespace pelf {
 
 namespace hana = boost::hana;
 
 
-/**  
+/**
  *
  * Checks if the template parameter Container has an alias named value_type
- *  and it also checks if that type is convertible to Type */  
-template <class Container, class Type>
-concept container_and_convertible_v = 
+ *  and it also checks if that type is convertible to Type */
+template<class Container, class Type>
+concept container_and_convertible_v =
   std::is_nothrow_convertible_v<typename Container::value_type, Type>;
-
-/** @brief Checks whether the template argument has a data method */
-template <class Container>
-concept has_data_method_v = requires (Container c){
-  c.data();
-};
 
 
 /** @brief Base class for Pe and Elf classes
@@ -46,12 +39,12 @@ concept has_data_method_v = requires (Container c){
  *  @tparam Container The type of the container used to store the file content
  *  that needs to be parsed
  *
- *  @tparam Derived The type of the inherited class 
+ *  @tparam Derived The type of the inherited class
  *  */
-template <class Container, template<typename> class Derived>
-class Pelf {
+template<class Container, template<typename> class Derived>
+class Pelf
+{
 public:
-  
   /** @brief Pelf constructor
    *
    *  Takes as a parameter a container that satisfies the concept
@@ -59,89 +52,123 @@ public:
    *  and has_data_method_v<Container>
    *
    *  @param data The data to be parsed
-   *  * */ 
-  constexpr explicit Pelf(Container data)
-    noexcept 
-    requires container_and_convertible_v<Container, unsigned char> && 
-      has_data_method_v<Container>;
+   *  * */
+  constexpr explicit Pelf(Container data) noexcept
+    requires container_and_convertible_v<Container, unsigned char>;
 
 
   /** @brief Returns the original data passed to the Pe/Elf constructor
    *
-   *  @return The original data stored in `mData` 
+   *  @return The original data stored in `mData`
    *
    * */
   [[nodiscard]] constexpr auto getRawData() const noexcept -> Container;
-
-
 
 
 protected:
   Container mData; /**< Raw data from the file to Îbe parsed */
 
   /** @brief Parses the PE/ELF headers and sections
-   * 
+   *
    *
    *  @return Void.
    *
    * */
   constexpr auto parse() -> void;
 
-	 
- 	template <class Struct>
-  constexpr auto getStruct(std::size_t offset) -> Struct; 
 
+  /** @brief Reads the contents from `mData` at the index `offset` into the struct passed by reference 
+   *
+   *  @return Void
+   * */
+  template <class Header>
+  constexpr auto readHeader(Header& header, const std::ptrdiff_t offset) -> void;
+
+  /** @brief Returns an Struct filled with the data from `mData` from index `offset`
+   *  
+   *  This function is only used if parsing is done at compile time, otherwise std::copy() is used
+   *
+   *
+   *  @return `Struct`
+   *  */
+  template<class Struct>
+  constexpr auto getStruct(std::size_t offset) -> Struct;
 };
 
 
-template <class Container, template<typename> class Derived>
-constexpr Pelf<Container, Derived>::Pelf(Container data) 
-  noexcept
-  requires container_and_convertible_v<Container, unsigned char> && 
-    has_data_method_v<Container> :
-  mData(std::move(data)) {}
+template<class Container, template<typename> class Derived>
+constexpr Pelf<Container, Derived>::Pelf(Container data) noexcept
+  requires container_and_convertible_v<Container, unsigned char>
+  : mData(std::move(data)) {}
 
 
-
-template <class Container, template<typename> class Derived>
-constexpr auto Pelf<Container, Derived>::getRawData() const noexcept -> Container {
+template<class Container, template<typename> class Derived>
+constexpr auto Pelf<Container, Derived>::getRawData() const noexcept -> Container
+{
   return mData;
 }
 
 
+template<class Container, template<typename> class Derived>
+constexpr auto Pelf<Container, Derived>::parse() -> void
+{
 
-template <class Container, template<typename> class Derived>
-constexpr auto Pelf<Container, Derived>::parse() -> void {
-  
-  auto& pelf = static_cast<Derived<Container>& >(*this);
+  auto &pelf = static_cast<Derived<Container> &>(*this);
 
   if (!pelf.checkFileSize()) {
-    throw PelfException{"Invalid File Size"};
+    throw PelfException{ "Invalid File Size" };
   }
-  
+
   if (!pelf.checkSignatures()) {
-    throw PelfException{"Invalid signatures!"};
+    throw PelfException{ "Invalid signatures!" };
   }
 
   pelf.parseHeaders();
-  
-  //pelf.parseSections();
 
-
+  // pelf.parseSections();
 }
 
+
+
 template <class Container, template<typename> class Derived>
-template <class Struct>
-constexpr auto Pelf<Container, Derived>::getStruct(std::size_t offset) 
-  -> Struct {
+template <class Header>
+constexpr auto Pelf<Container, Derived>::readHeader(Header& header, 
+    const std::ptrdiff_t offset) -> void {
+
+  if (offset < 0) {
+    throw PelfException{"Invalid offset (negative), while parsing headers"};
+  }
+
+  if (std::is_constant_evaluated()) {
+    header = this->template getStruct<Header>(static_cast<std::size_t>(offset));
+  } else {
+
+    assert(std::is_trivially_copyable<Header>::value);
+
+    if (static_cast<std::size_t>(offset) + sizeof(header) < this->mData.size()) {
+      std::copy(this->mData.begin() + offset,
+          this->mData.begin() + sizeof(header),
+          reinterpret_cast<char *>(&header));
+    } else {
+      throw PelfException{ "Invalid Header" };
+    }
+  } 
+}
+
+
+template<class Container, template<typename> class Derived>
+template<class Struct>
+constexpr auto Pelf<Container, Derived>::getStruct(std::size_t offset)
+  -> Struct
+{
 
   Struct s{};
 
   hana::for_each(hana::keys(s), [&](auto key) {
-
-    auto& member = hana::at_key(s, key);
+    auto &member = hana::at_key(s, key);
+    
     #pragma unroll
-    for (std::size_t i{}; i < sizeof(member); ++i){
+    for (std::size_t i{}; i < sizeof(member); ++i) {
       if (sizeof(member) > 1) {
         member <<= 8;
       }
@@ -152,8 +179,8 @@ constexpr auto Pelf<Container, Derived>::getStruct(std::size_t offset)
   });
 
   return s;
-}  
+}
 
-} // end of namespace pelf
+}// end of namespace pelf
 
 #endif
