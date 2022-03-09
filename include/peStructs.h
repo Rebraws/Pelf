c3b3f2325a3545d5bf028b2b4adafd812b591352
Rebraws <Rebraws@protonmail.com>
2022-03-09 01:00:55 -0300

    Formatting

--- include/peStructs.h
+++ include/peStructs.h
@@ -1,6 +1,6 @@
-/** @file peStructs.h 
- *  
- *  @brief Pe headers and sections structs 
+/** @file peStructs.h
+ *
+ *  @brief Pe headers and sections structs
  *
  *  This file contains definition of structs that represent
  *  the internals of the PE file format
@@ -22,147 +22,156 @@ namespace pelf {
 namespace hana = boost::hana;
 
 /* Windows typedefs */
-using WORD 			= std::uint16_t;
-using DWORD 		= std::uint32_t;
-using BYTE 			= std::uint8_t;
+using WORD = std::uint16_t;
+using DWORD = std::uint32_t;
+using BYTE = std::uint8_t;
 using ULONGLONG = std::uint64_t;
 
-inline constexpr std::uint8_t IMAGE_NUMBER_OF_DIRECTORY_ENTRIES{16};
+inline constexpr std::uint8_t IMAGE_NUMBER_OF_DIRECTORY_ENTRIES{ 16 };
 
 
-
-/** @brief Struct that represents the COFF header format. It's 32/64 bit independent header
- *  
+/** @brief Struct that represents the COFF header format. It's 32/64 bit
+ * independent header
+ *
  *
  * */
-#pragma pack(push, 1)     // Disable padding 
-struct IMAGE_FILE_HEADER {
+#pragma pack(push, 1)// Disable padding
+struct IMAGE_FILE_HEADER
+{
   /** @brief Macro that defines members of the structure
    *
-   *  
+   *
    * */
-	BOOST_HANA_DEFINE_STRUCT(IMAGE_FILE_HEADER,
-  	(WORD,  Machine),          
-  	(WORD,  NumberOfSections),
-  	(DWORD, TimeDateStamp),
-  	(DWORD, PointerToSymbolTable),
-  	(DWORD, NumberOfSymbols),
-  	(WORD,  SizeOfOptionalHeader),
-  	(WORD,  Characteristics)
-	);
-
+  BOOST_HANA_DEFINE_STRUCT(IMAGE_FILE_HEADER,
+    (WORD, Machine),
+    (WORD, NumberOfSections),
+    (DWORD, TimeDateStamp),
+    (DWORD, PointerToSymbolTable),
+    (DWORD, NumberOfSymbols),
+    (WORD, SizeOfOptionalHeader),
+    (WORD, Characteristics));
 };
 #pragma pack(pop)
 
 
-#pragma pack(push, 1)   // disable padding
-struct IMAGE_DATA_DIRECTORY {
-  BOOST_HANA_DEFINE_STRUCT(IMAGE_DATA_DIRECTORY,  
-		(DWORD,   VirtualAddress),
-  	(DWORD,	 Size)
-	);
-}; 
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct StandardCoffFields {
-	BOOST_HANA_DEFINE_STRUCT(StandardCoffFields,
-    (WORD,                 Magic),
-    (BYTE,                 MajorLinkerVersion),
-    (BYTE,                 MinorLinkerVersion),
-    (DWORD,                SizeOfCode),
-    (DWORD,                SizeOfInitializedData),
-    (DWORD,                SizeOfUninitializedData),
-    (DWORD,                AddressOfEntryPoint),
-    (DWORD,                BaseOfCode)
-	);
+#pragma pack(push, 1)// disable padding
+struct IMAGE_DATA_DIRECTORY
+{
+  BOOST_HANA_DEFINE_STRUCT(IMAGE_DATA_DIRECTORY,
+    (DWORD, VirtualAddress),
+    (DWORD, Size));
 };
 #pragma pack(pop)
 
 #pragma pack(push, 1)
-struct WindowsSpecificFields {
+struct StandardCoffFields
+{
+  BOOST_HANA_DEFINE_STRUCT(StandardCoffFields,
+    (WORD, Magic),
+    (BYTE, MajorLinkerVersion),
+    (BYTE, MinorLinkerVersion),
+    (DWORD, SizeOfCode),
+    (DWORD, SizeOfInitializedData),
+    (DWORD, SizeOfUninitializedData),
+    (DWORD, AddressOfEntryPoint),
+    (DWORD, BaseOfCode));
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct WindowsSpecificFields
+{
   BOOST_HANA_DEFINE_STRUCT(WindowsSpecificFields,
-		
-    (ULONGLONG,   ImageBase),
-		(DWORD,       SectionAlignment),
-		(DWORD,       FileAlignment),
-		(WORD,        MajorOperatingSystemVersion),
-		(WORD,        MinorOperatingSystemVersion),
-		(WORD,        MajorImageVersion),
-		(WORD,        MinorImageVersion),
-		(WORD,        MajorSubsystemVersion),
-		(WORD,        MinorSubsystemVersion),
-		(DWORD,       Win32VersionValue),
-		(DWORD,       SizeOfImage),
-		(DWORD,       SizeOfHeaders),
-		(DWORD,       CheckSum),
-		(WORD,        Subsystem),
-		(WORD,        DllCharacteristics),
-		(ULONGLONG,   SizeOfStackReserve),
-		(ULONGLONG,   SizeOfStackCommit),
-		(ULONGLONG,   SizeOfHeapReserve),
-		(ULONGLONG,   SizeOfHeapCommit),
-		(DWORD,       LoaderFlags),
-		(DWORD,       NumberOfRvaAndSizes)
- );
+
+    (ULONGLONG, ImageBase),
+    (DWORD, SectionAlignment),
+    (DWORD, FileAlignment),
+    (WORD, MajorOperatingSystemVersion),
+    (WORD, MinorOperatingSystemVersion),
+    (WORD, MajorImageVersion),
+    (WORD, MinorImageVersion),
+    (WORD, MajorSubsystemVersion),
+    (WORD, MinorSubsystemVersion),
+    (DWORD, Win32VersionValue),
+    (DWORD, SizeOfImage),
+    (DWORD, SizeOfHeaders),
+    (DWORD, CheckSum),
+    (WORD, Subsystem),
+    (WORD, DllCharacteristics),
+    (ULONGLONG, SizeOfStackReserve),
+    (ULONGLONG, SizeOfStackCommit),
+    (ULONGLONG, SizeOfHeapReserve),
+    (ULONGLONG, SizeOfHeapCommit),
+    (DWORD, LoaderFlags),
+    (DWORD, NumberOfRvaAndSizes));
 };
 #pragma pack(pop)
 
-struct OptionalHeader {
-  StandardCoffFields      mScf{};
-  WindowsSpecificFields   mWsf{};
-  std::array<IMAGE_DATA_DIRECTORY, IMAGE_NUMBER_OF_DIRECTORY_ENTRIES> mDataDirectories{};
+struct OptionalHeader
+{
+  StandardCoffFields mScf{};
+  WindowsSpecificFields mWsf{};
+  std::array<IMAGE_DATA_DIRECTORY, IMAGE_NUMBER_OF_DIRECTORY_ENTRIES>
+    mDataDirectories{};
 };
 
 
-struct PeHeaders {
-  IMAGE_FILE_HEADER mCoffHeader{}; /**< Struct that represents the coff header */
-  OptionalHeader mOptionalHeader;  /**< Struct that represents the optional header */
+struct PeHeaders
+{
+  IMAGE_FILE_HEADER
+    mCoffHeader{}; /**< Struct that represents the coff header */
+  OptionalHeader mOptionalHeader; /**< Struct that represents the
+                                    optional header */
 
   /* Methods to make access to headers more readable for the user */
-  [[nodiscard]] constexpr auto getCoffHeader() const noexcept 
-    -> IMAGE_FILE_HEADER {
+  [[nodiscard]] constexpr auto getCoffHeader() const noexcept
+    -> IMAGE_FILE_HEADER
+  {
     return mCoffHeader;
   }
-  
-  [[nodiscard]] constexpr auto getOptionalHeader() const noexcept -> OptionalHeader {
+
+  [[nodiscard]] constexpr auto getOptionalHeader() const noexcept
+    -> OptionalHeader
+  {
     return mOptionalHeader;
   }
 
   [[nodiscard]] constexpr auto getStandardCoffFields() const noexcept
-    -> StandardCoffFields {
+    -> StandardCoffFields
+  {
     return mOptionalHeader.mScf;
-  } 
+  }
 
   [[nodiscard]] constexpr auto getWindowsSpecificFields() const noexcept
-    -> WindowsSpecificFields {
+    -> WindowsSpecificFields
+  {
     return mOptionalHeader.mWsf;
   }
 
-  [[nodiscard]] constexpr auto getDataDirectories() const noexcept {
-      return mOptionalHeader.mDataDirectories;
-    }
-
+  [[nodiscard]] constexpr auto getDataDirectories() const noexcept
+  {
+    return mOptionalHeader.mDataDirectories;
+  }
 };
 
 #pragma pack(push, 1)
-struct IMAGE_SECTION_HEADER {
+struct IMAGE_SECTION_HEADER
+{
   BOOST_HANA_DEFINE_STRUCT(IMAGE_SECTION_HEADER,
-    (ULONGLONG,  Name),
-    (ULONGLONG,  PhysAddressAndVirtSize),
+    (ULONGLONG, Name),
+    (ULONGLONG, PhysAddressAndVirtSize),
     (DWORD, VirtualAddress),
     (DWORD, SizeOfRawData),
     (DWORD, PointerToRawData),
     (DWORD, PointerToRelocations),
     (DWORD, PointerToLinenumbers),
-    (WORD,  NumberOfRelocations),
-    (WORD,  NumberOfLinenumbers),
-    (DWORD, Characteristics)
-  );
-}; 
+    (WORD, NumberOfRelocations),
+    (WORD, NumberOfLinenumbers),
+    (DWORD, Characteristics));
+};
 #pragma pack(pop)
 
 
-} // namespace end 
+}// namespace pelf
 
 #endif
